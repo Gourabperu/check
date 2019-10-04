c858597b886a6fc72a79d4177c5d91b7a74e11e0
Gourabperu <gourabperu@outlook.com>
2019-10-04 13:42:44 -0500

    first vertion

---
+++ index.js
@@ -0,0 +1,37 @@
+Culqi.publicKey = 'pk_test_Og9KU3vS6PKZ50oL';
+
+
+  Culqi.settings({
+    title: 'Culqi Store',
+    currency: 'PEN',
+    description: 'Polo/remera Culqi lover',
+    amount: 100
+  });
+
+$('#buyButton').on('click', function(e) {
+    // Abre el formulario con la configuración en Culqi.settings
+    Culqi.open();
+    e.preventDefault();
+});
+
+function culqi() {
+    if (Culqi.token) { // ¡Objeto Token creado exitosamente!
+        var token = Culqi.token.id;
+        alert('Se ha creado un token:' + token);
+        // Aqui enviar token Id a servidor para crear cargo...
+    }
+    else if (Culqi.order) {
+        console.log(Culqi.order)
+  
+        alert('Se ha elegido el metodo PagoEfectivo');
+  
+         /* Aqui enviar al servidor el order Id y asociarlo al detalle de tu venta.
+            Además, tu venta en tu comercio debe quedar estado pendiente.
+          */
+    }
+    else { // ¡Hubo algún problema!
+        // Mostramos JSON de objeto error en consola
+        console.log(Culqi.error);
+        alert(Culqi.error.user_message);
+    }
+  };
